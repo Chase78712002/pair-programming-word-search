169519cecbee3b02150dfe5adf870ede7724603a
Alvin Lin <alvinch.lin@gmail.com>
2021-04-07 22:45:21 +0000

    fixed the bug in the wordseach.js by moving the return false out of the loop.

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,14 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  console.log(horizontalJoin);
+  for (const l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
+    } else {
+      continue;
     }
-}
+  }
+  return false;
+};
 
-module.exports = wordSearch
+module.exports = wordSearch;
